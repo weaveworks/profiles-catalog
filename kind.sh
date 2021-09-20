9a6682bc630e5fc4991d5603c53695624b39b351
Andrew Stangl <andrewstangl@gmail.com>
2021-09-20 12:04:39 +0200

    Better output

--- kind.sh
+++ kind.sh
@@ -20,6 +20,7 @@ KIND_CLUSTER=mgmt
 #Install WeaveGitops:
 # - check if installed, if not install from GH release:
 if [[ ! -x $(which wego) ]]; then
+    echo "wego binary not found, installing ..."
     curl -L "https://github.com/weaveworks/weave-gitops/releases/download/v${WEGO_VERSION}/wego-$(uname)-$(uname -m)" -o wego
     chmod +x wego
     sudo mv ./wego /usr/local/bin/wego
@@ -29,6 +30,7 @@ fi
 #Install ProfileCTL:
 # - check if installed, if not install from GH release:
 if [[ ! -x $(which pctl) ]]; then
+    echo "pctl binary not found, installing ..."
     OS=$(uname | tr '[:upper:]' '[:lower:]')
     wget "https://github.com/weaveworks/pctl/releases/download/v${PCTL_VERSION}/pctl_${OS}_amd64.tar.gz"
     tar xvfz pctl_${OS}_amd64.tar.gz
@@ -36,12 +38,16 @@ if [[ ! -x $(which pctl) ]]; then
     pctl --version
 fi
 
+echo "Creating kind management cluster ..."
 kind get clusters | grep ${KIND_CLUSTER} || kind create cluster --config kind-cluster-with-extramounts.yaml --name ${KIND_CLUSTER}
 
+echo "Exporting kind management cluster kubeconfig ..."
 kind get kubeconfig --name ${KIND_CLUSTER} > ${KIND_CLUSTER}.kubeconfig
 
+echo "Initialising docker provider in kind management cluster ..."
 clusterctl init --infrastructure docker --wait-providers || true
 
+echo "Generating manifests for workload cluster, and applying them ..."
 clusterctl generate cluster ${WORKLOAD_CLUSTER} \
   --flavor development \
   --kubernetes-version v1.21.1 \
@@ -50,6 +56,8 @@ clusterctl generate cluster ${WORKLOAD_CLUSTER} \
   | kubectl apply -f -
 
 READY=false
+set +e
+echo "Checking whether workload cluster in ready state..."
 until [[ $READY == "true" ]]; do
     clusterctl describe cluster ${WORKLOAD_CLUSTER} 2>&1 | grep -i "/${WORKLOAD_CLUSTER}" | grep True
     if [ $? == 0 ]; then
@@ -66,11 +74,14 @@ until [[ $READY == "true" ]]; do
         sleep 3
     fi
 done
+set -e
 
 echo "Currently running machines:"
 kubectl get machines
 
 READY=false
+set +e
+echo "Checking whether machines in workload cluster in RUNNING state..."
 until [[ $READY == "true" ]]; do
     kubectl get machines -o jsonpath='{.items[].status.phase}' | grep Running
     if [ $? == 0 ]; then
@@ -80,6 +91,7 @@ until [[ $READY == "true" ]]; do
         sleep 3
     fi
 done
+set -e
 
 clusterctl get kubeconfig ${WORKLOAD_CLUSTER} > ${WORKLOAD_CLUSTER}.kubeconfig
 
@@ -88,6 +100,7 @@ sed -i -e "s/server:.*/server: https:\/\/$(docker port ${WORKLOAD_CLUSTER}-lb 64
 
 #kubectl --kubeconfig=./${WORKLOAD_CLUSTER}.kubeconfig apply -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml
 
+echo "Installing Cilium CNI, via Helm"
 helm repo add cilium https://helm.cilium.io/
 helm status --kubeconfig=./${WORKLOAD_CLUSTER}.kubeconfig -n kube-system cilium || \
     helm install cilium cilium/cilium --version 1.9.10 \
@@ -106,7 +119,14 @@ helm status --kubeconfig=./${WORKLOAD_CLUSTER}.kubeconfig -n kube-system cilium 
 
 export KUBECONFIG=./${WORKLOAD_CLUSTER}.kubeconfig
 
+echo "Pulling profiles controller from docker hub"
+docker pull weaveworks/profiles-controller:v0.2.0
+
+echo "Loading profile controller images into workload cluster nodes"
 kind load docker-image --name ${WORKLOAD_CLUSTER} weaveworks/profiles-controller:v0.2.0
 
+echo "Installing WeaveGitops"
 wego gitops install
+
+echo "Installing profile-controller"
 pctl install --flux-namespace wego-system
