b27101f6e56f4c1e807e0d6afbabc1c8dbb86fcc
Filipe Sequeira <filipe@weave.works>
2021-09-23 13:09:52 +0100

    check if cluster is bootstraped

--- .bin/catalog-install.sh
+++ .bin/catalog-install.sh
@@ -8,29 +8,13 @@ set -e
 set -o pipefail
 
 export PATH=$PATH
-
-BINDIR="${PWD}/.bin"
-REPODIR="${PWD}/.repo"
-PROFILE=""
-PROFILE_DIR=""
-TEST_REPO_USER=ww-customer-test
-TEST_REPO=profile-test-repo 
+FLUX_NAMESPACE=wego-system
 CATALOG_REPO_URL=ssh://git@github.com/weaveworks/profiles-catalog.git 
 
-if [ ! -n "$1"  ]; then
-    echo "Please supply profile name"
-    exit 10
+if ! kubectl get secret flux-system -n ${FLUX_NAMESPACE} 2>&1 >/dev/null; then
+    echo "Cluster not bootraped"
+    exit 1
 fi
-
-if [ ! -n "$GITHUB_TOKEN"  ]; then
-    echo "GITHUB_TOKEN has not been configured"
-    echo "Please add create a token https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
-    exit 10
-fi
-
-echo "Install secret to use with catalog repo"
-flux create secret git catalog-secret --url ${CATALOG_REPO_URL}  
-
 
 echo "Install catalog to cluster"
 cat <<EOF | kubectl apply -f -
@@ -38,9 +22,10 @@ apiVersion: weave.works/v1alpha1
 kind: ProfileCatalogSource
 metadata:
   name: enterprise-profiles
+  namespace: ${FLUX_NAMESPACE}
 spec:
   repositories:
   - url: ${CATALOG_REPO_URL}
     secretRef:
-      name: catalog-secret
+      name: flux-system
 EOF
