6d344a0683aab2d27426974186d02f5307f8cd6f
Tom Huang <tom.huang@weave.works>
2021-10-15 12:09:30 -0400

    fix script

--- .bin/update-chart-versions.sh
+++ .bin/update-chart-versions.sh
@@ -2,19 +2,24 @@ PROFILE_VERSION_ANNOTATION=$1
 
 for f in $(ls */profile.yaml); do
     CHART_URLS=$(yq e '.spec.artifacts.[].chart.url | select(. != null)' ${PWD}/${f})
-    CHART_NAMES=$(yq e '.spec.artifacts.[].chart.name | select(. != null)' ${PWD}/${f})
+    CHART_NAMES=($(yq e '.spec.artifacts.[].chart.name | select(. != null)' ${PWD}/${f}))
     INDEX=0
+    BUMP_PROFILE_VERSION=0
     for i in ${CHART_URLS[@]}; do
         URL=$i    
         CHART=${CHART_NAMES[$INDEX]}
         ((INDEX++))
         helm repo add temp-repo $i --force-update        
         LATEST_VERSION=$(helm search repo temp-repo/$CHART -o yaml | yq e '.[0].version' -)
-        if [ $(yq e "(.spec.artifacts[] | select(.chart.name == \"$CHART\")).chart.version" ${PWD}/${f}) != $LATEST_VERSION ]; then
+        CUR_VERSION=$(yq e "(.spec.artifacts[] | select(.chart.name == \"$CHART\")).chart.version" ${PWD}/${f})
+        if [ $CUR_VERSION != $LATEST_VERSION ]; then
             yq e -i "(.spec.artifacts[] | select(.chart.name == \"$CHART\")).chart.version = \"$LATEST_VERSION\"" ${PWD}/${f}
-            VERSION=$(yq e ".metadata.annotations.\"$PROFILE_VERSION_ANNOTATION\"" ${PWD}/${f})
-            NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF++;print}')
-            yq e -i ".metadata.annotations.\"$PROFILE_VERSION_ANNOTATION\" = \"$NEXTVERSION\"" ${PWD}/${f}
+            BUMP_PROFILE_VERSION=1
         fi
     done
+    if [ $BUMP_PROFILE_VERSION == 1 ]; then
+        VERSION=$(yq e ".metadata.annotations.\"$PROFILE_VERSION_ANNOTATION\"" ${PWD}/${f})
+        NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF++;print}')
+        yq e -i ".metadata.annotations.\"$PROFILE_VERSION_ANNOTATION\" = \"$NEXTVERSION\"" ${PWD}/${f}
+    fi
 done
