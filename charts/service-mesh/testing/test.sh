25ab2a8956ded435c55708446b0d24ed8f768b03
steve-fraser <stevenrfraser7@gmail.com>
2021-10-29 14:58:18 -0400

    adding initial configs for linkerd sevice-mesh

--- charts/service-mesh/testing/test.sh
+++
@@ -1,7 +0,0 @@
-helm upgrade linkerd2 \
-    --set installNamespace=false \
-    --set identity.issuer.scheme=kubernetes.io/tls \
-    linkerd/linkerd2 \
-    --post-renderer ./hook.sh --debug \
-    --create-namespace \
-    -n linkerd
