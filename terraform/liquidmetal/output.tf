06d84b6265c64e87305b5b3038450a9bef6f3197
steve-fraser <stevenrfraser7@gmail.com>
2022-03-01 09:44:32 -0500

    testing initial target with just an echo

---
+++ terraform/liquidmetal/output.tf
@@ -0,0 +1,12 @@
+output "dhcp_ip" {
+  value = metal_device.dhcp_nat.network.0.address
+}
+output "host_ips" {
+  value = metal_device.host.*.network.0.address
+}
+
+output "terraform_state_location" {
+  value = <<EOV
+${path.cwd}/terraform.tfstate.d/${terraform.workspace}/terraform.tfstate
+EOV
+}
